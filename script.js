25ef453eabcd38cb3b1402deb7aaa79008e7ad8f
Abdale Hassan <cabdale989@gmail.com>
2021-04-07 15:44:00 -0500

    add random list to javaScript

--- script.js
+++ script.js
@@ -6,6 +6,17 @@ var startBtn = document.querySelector(".start-btn");
 var timerCount = 60
 var timer;
 
+
+var listEl = ["strings", "booleans", "alert", "numbers", 
+              "quotes", "curly brackets", "parentheses", 
+              "square brackets", "numbers and strings", 
+              "other Arrays", "all of the above", "commas", 
+              "javaScript", "terminal/bash", "for loops", "console.log"
+            ];
+var randomEl = listEl[Math.floor(Math.random() * listEl.length)];
+
+console.log("this is randomEl:>> "+ randomEl);
+
 function startButton(){
     startBtn.addEventListener("click", function(){
         
