90df3e355d7fc6284bb5ff7ac6241fcc822a647b
Abdale Hassan <cabdale989@gmail.com>
2021-04-07 19:03:46 -0500

    Create div elements in javaScript to store questions for the quiz

--- script.js
+++ script.js
@@ -3,19 +3,36 @@ var timerEl = document.querySelector(".time");
 var startBtn = document.querySelector(".start-btn");
 
 
-var timerCount = 60
+var timerCount = 0
 var timer;
 
+var body = document.body;
+var dataType = document.createElement("div");
+var curlyBrackets = document.createElement("div");
+var javaArray = document.createElement("div");
+var stringValue = document.createElement("div");
+var consoleLog = document.createElement("div");
 
-var listEl = ["strings", "booleans", "alert", "numbers", 
+dataType.textContent = "commonly used data types DO NOT include:";
+curlyBrackets.textContent  = "The condition in an if/else statement is enclosed within____.";
+javaArray.textContent = "Arrays in JavaScript can be used to store_____.";
+stringValue.textContent = "String value must be enclosed within ____ when  being assigned to variable.";
+consoleLog.textContent = "A very useful tool used during development and debugging for printing content to the debugger is:";
+//====
+
+
+
+
+var liItems = ["strings", "booleans", "alert", "numbers", 
               "quotes", "curly brackets", "parentheses", 
               "square brackets", "numbers and strings", 
               "other Arrays", "all of the above", "commas", 
               "javaScript", "terminal/bash", "for loops", "console.log"
             ];
-var randomEl = listEl[Math.floor(Math.random() * listEl.length)];
+var randomList = liItems[Math.floor(Math.random() * liItems.length)];
 
-console.log("this is randomEl:>> "+ randomEl);
+list.textContent = "1: " + randomList;
+
 
 function startButton(){
     startBtn.addEventListener("click", function(){
